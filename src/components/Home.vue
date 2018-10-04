85070b98fcb5a5a92b42ce2a072bcafbbd8a5c29
Thom David <thomdavid74@gmail.com>
2018-10-04 15:20:45 -0700

    Available funds display and test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,19 @@
 <template>
-  <h1>The Home Component</h1>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may Save & Load your Data</h6>
+    <h6>Click on 'End Day' to begin a new day</h6>
+    <hr>
+    <p>Your Available Funds: US$ {{ Number.parseFloat(funds).toFixed(2) }}</p>
+  </div>
 </template>
 
+<script>
+  export default {
+    computed: {
+      funds() {
+        return this.$store.getters.funds
+      }
+    }
+  }
+</script>
