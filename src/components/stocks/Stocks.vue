ac3c9b0fceb64e6023f96143fbe808ca828775c8
Thom David <thomdavid74@gmail.com>
2018-10-04 08:26:07 -0700

    Dummy stock data and formatting Stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>The stocks/Stocks Component</h1>
+  <div>
+    <app-stock v-for="(stock, id) in stocks" v-bind:key="id"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 79 },
+          { id: 2, name: 'Google', price: 200 },
+          { id: 3, name: 'Apple', price: 231 },
+          { id: 4, name: 'Twitter', price: 29 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
